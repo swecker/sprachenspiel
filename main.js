749f7d05ca87dac11aa80a2b5d7a2feb1a4fd862
Samuel Wecker <wecker.samuel@gmail.com>
2012-03-14 18:31:31 -0600

    Create basis for game page.

---
+++ main.js
@@ -0,0 +1,16 @@
+var grid = [[0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0]];
+
+var rachael = [123,321,213];
+
+$(document).ready(function() {
+
+    var l = length(rachael.length();
+    for( i=0; i<l; i++ ) {
+      alert( rachael[i] )
+    }
+
+});
